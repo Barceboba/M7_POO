5508a9034db4268c40ab6fbc0c8dffd081705efa
Barceboba <miquel@barceboba.com>
2021-07-07 15:32:18 +0200

    comentat

--- Nivell_1/js/Products.js
+++ Nivell_1/js/Products.js
@@ -14,15 +14,13 @@ document.getElementById('product-form').addEventListener('submit', function (e) 
     const name = document.getElementById('name').value;
     const price = document.getElementById('price').value;
     const year = document.getElementById('year').value;
-
-
+    //Pintar entrada
     console.log('Nom del producte: ' + name + '. Preu del producte: ' + price + ' €. Any de fabricació: ' + year + '.');
-    console.log(new Product(name, price, year));
-
-    
+    //Nou objecte
     const product = new Product(name, price, year);
-    
+    //Afegir a l'array
     dataBase.push(product);
+    //Array per consola
     console.log(dataBase);
     e.preventDefault();
 });
