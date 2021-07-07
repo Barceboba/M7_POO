f30d485b49506f0b4578a5538a04a4c67a8b49a4
Barceboba <miquel@barceboba.com>
2021-07-07 17:44:16 +0200

    delete implementat

--- Nivell_2/js/Products.js
+++ Nivell_2/js/Products.js
@@ -6,22 +6,53 @@ class Product {
         this.year = year;
     }
 }
-let dataBase = [];
+class UI {
+    addProduct(product) {
+        const productList = document.getElementById('product-list');
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div div class="card text-center mb-4" >
+                <div class="card-body">
+                    <strong>Nom del producte</strong>: ${product.name}
+                    <strong>Preu del producte</strong>: ${product.price}
+                    <strong>Any del producte</strong>: ${product.year}
+                    <a href="#" class="btn btn-danger" name="delete">Borrar</a>
+                </div>
+        </div>
+            `;
+        productList.appendChild(element);
+        this.resetForm();
+    }
+    resetForm() {
+        document.getElementById('product-form').reset();
+    }
+    deleteProduct(element) {
+        if (element.name === 'delete') {
+            element.parentElement.parentElement.parentElement.remove();
+        }
+    }
 
+    showMessage() {
+
+    }
+}
 //Events del DOM
 document.getElementById('product-form').addEventListener('submit', function (e) {
 
     const name = document.getElementById('name').value;
     const price = document.getElementById('price').value;
     const year = document.getElementById('year').value;
-    //Pintar entrada
-    console.log('Nom del producte: ' + name + '. Preu del producte: ' + price + ' €. Any de fabricació: ' + year + '.');
+
     //Nou objecte
     const product = new Product(name, price, year);
-    //Afegir a l'array
-    dataBase.push(product);
-    //Array per consola
-    console.log(dataBase);
+    const ui = new UI();
+    ui.addProduct(product);
+    ui.resetForm();
     e.preventDefault();
+});
+
+document.getElementById('product-list').addEventListener('click', function (e) {
+    const ui = new UI();
+    ui.deleteProduct(e.target);
 });
 
